a7c91e5ccb22f0a179259f1cfbe693c293cd94f5
Valentin Drenkov <drenkovvv@gmail.com>
2024-10-16 12:10:50 +0300

    Movies grid component created.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <div className="container">
         <Header></Header>
       </div>
+
+      <MoviesGrid></MoviesGrid>
 
       <Footer></Footer>
     </div>
