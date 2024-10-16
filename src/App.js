8ca249112abe22d3981a1a5db094c8a0d212fbd0
Valentin Drenkov <drenkovvv@gmail.com>
2024-10-16 14:32:39 +0300

    Routing added.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Header></Header>
+        <Header />
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<MoviesGrid />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </Router>
       </div>
 
-      <MoviesGrid></MoviesGrid>
-
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 }
