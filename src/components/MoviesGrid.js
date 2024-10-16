3d6fc92bc21e67d943913eda948a8b0f58317799
Valentin Drenkov <drenkovvv@gmail.com>
2024-10-16 13:04:21 +0300

    Search bar added.

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -4,6 +4,15 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   useEffect(() => {
     fetch("movies.json")
@@ -12,10 +21,20 @@ export default function MoviesGrid() {
   }, []);
 
   return (
-    <div className="movies-grid">
-      {movies.map((movie) => (
-        <MovieCard movie={movie} key={movie.id}></MovieCard>
-      ))}
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
     </div>
   );
 }
